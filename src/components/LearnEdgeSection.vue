fc6a6243b555eac66149e0bec8be6f40dbe57826
Navid Bonyadi <navidprinceofpersia@gmail.com>
2023-12-13 20:19:05 +0330

    created gradient EDGE under tailwind

--- src/components/LearnEdgeSection.vue
+++ src/components/LearnEdgeSection.vue
@@ -5,11 +5,15 @@
   <div class="bg-slate-200 pt-24 pb-10">
     <div class="flex  px-10">
       <div class="">
-        <h2 class="text-5xl text-slate-700 mb-5">Learn
-          <span class="relative">EDGE
-            <span
-              class="bg-gradient-to-r from-red-400 via-yellow-400 to-emerald-400 absolute top-12 mt-1 right-0 w-36 h-3 custom-border "></span>
-          </span> Cases
+        <h2 class="text-5xl text-slate-700 mb-5 flex">Learn
+          <div class="relative px-2">EDGE
+            <div class="absolute top-13 left-0">
+              <span
+              class="flex justify-center items-center inner bg-gradient-to-r from-red-400 via-yellow-400 to-emerald-400 w-36 h-4 relative rounded-full before:block before:absolute before:bg-slate-200 before:rotate-2 before:translate-y-2 before:w-full before:h-2 after:block after:absolute after:bg-slate-200 after:-rotate-2 after:-translate-y-2 after:w-full after:h-2">
+            </span>
+            </div>
+            
+          </div> Cases
         </h2>
         <p class="text-xs text-slate-400 font-sans w-96">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -21,7 +25,8 @@
           Quos eos voluptate aliquam nesciunt
           hic iste laborum velit optio.
         </p>
-        <div class="bg-slate-50 shadow-md px-4 py-2 mt-5 text-sm text-slate-400 font-sans w-80 flex items-center justify-between rounded-full">
+        <div
+          class="bg-slate-50 shadow-md px-4 py-2 mt-5 text-sm text-slate-400 font-sans w-80 flex items-center justify-between rounded-full">
           What do you want to learn?
           <span class="bg-emerald-400 cursor-pointer px-6 py-1 rounded-full text-slate-50 text-xs">search</span>
         </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,92 @@ import HandSection from '../components/HandSection.vue'
   <TheNavbar />
   <LearnEdgeSection />
   <HandSection />
+  <div class="our-course bg-slate-200">
+    <div class="content">
+      <h2 class="title">Our Courses</h2>
+      <ul class="cards">
+        <li class="bg-slate-100">
+          <div class="image bg-slate-50"><span>IMAGE</span></div>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. adipisicing elit.</p>
+        </li>
+        <li class="bg-slate-100">
+          <div class="image bg-slate-50"><span>IMAGE</span></div>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. adipisicing elit.</p>
+        </li>
+        <li class="bg-slate-100">
+          <div class="image bg-slate-50"><span>IMAGE</span></div>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. adipisicing elit.</p>
+        </li>
+   <li class="loading">
+<div class="image"></div>
+   </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
-<style></style>
+<style>
+.our-course {
+
+  padding: 1.5rem 0;
+  min-height: 50vh;
+}
+.title {
+  font-size: 1.4rem;
+  margin-bottom: 12px;
+}
+.content {
+  margin: 0 auto;
+  width: 90%;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+  /* justify-content: center; */
+  align-items: center;
+  gap: 15px;
+}
+
+.cards li {
+  /* background-color: antiquewhite; */
+  padding: 8px;
+  width: 230px;
+  height: 300px;
+  border-radius: 8px;
+}
+.loading {
+  background-color: antiquewhite;
+  padding: 8px;
+  width: 230px;
+  height: 300px;
+  border-radius: 8px;
+}
+.loading .image {
+  width: 100%;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: cadetblue;
+  border-radius: 4px;
+  margin-bottom: 10px;
+}
+.cards li .image {
+  width: 100%;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* background-color: cadetblue; */
+  border-radius: 4px;
+  margin-bottom: 10px;
+}
+.cards li .image span {
+  font-size: 1.6rem;
+ }
+p {
+  font-size: 0.9rem;
+  font-family: sans-serif;
+}
+</style>
