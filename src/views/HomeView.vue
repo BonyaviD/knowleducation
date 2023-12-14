29c16ad58cebbc9bde23b1c2293096313d93cd40
Navid Bonyadi <navidprinceofpersia@gmail.com>
2023-12-14 17:20:23 +0330

    created Hand in footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -39,8 +39,14 @@ import CourseSection from '../components/CourseSection.vue'
       </div>
     </div>
     <div class="footer bg-emerald-400 mt-8 pt-1 h-20 rounded-t-3xl">
-      <div class="footer-two bg-slate-900 h-20 rounded-t-3xl flex items-center justify-center text-slate-400 text-xs">
-        <h4>Give me FIVE</h4>
+      <div class="footer-two group bg-slate-900 h-20 rounded-t-3xl flex items-center justify-center text-slate-400 text-xs">
+        <h4 class="relative">
+          <span class="">Give me FIVE</span>
+          <img
+           class="absolute -top-1 -right-6 w-5 cursor-pointer group-hover:w-6 group-hover:-right-7 group-hover:-top-1.5 active:scale-75" 
+           src="../assets/img/give-hand.png" 
+           alt="">
+        </h4>
       </div>
     </div>
   </div>
@@ -56,4 +62,5 @@ import CourseSection from '../components/CourseSection.vue'
 .footer-two {
   border-radius: 50% 50% 0 0;
 }
+
 </style>
