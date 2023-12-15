9617c7139f100b144187888792495cfbf4e7bd45
Navid Bonyadi <navidprinceofpersia@gmail.com>
2023-12-15 21:34:54 +0330

    test useintersectionobserver

--- src/components/Cards/LoadedCourseCard.vue
+++ src/components/Cards/LoadedCourseCard.vue
@@ -1,28 +1,38 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue'
+import { useIntersectionObserver } from '@vueuse/core'
 
-import { defineProps, onMounted } from 'vue';
+const root = ref(null)
+const target = ref(null)
+const isVisible = ref(false)
 
-const props =  defineProps({
-  card: Object
+const { isActive, pause, resume } = useIntersectionObserver(
+  target,
+  ([{ isIntersecting }]) => {
+    isVisible.value = isIntersecting
+  },
+  { root },
+)
+
+watch(()=>isVisible.value, ()=>{
+  console.log(isVisible.value)
 })
 
-onMounted(async ()=>{
-  console.log(props)
-})
 </script>
 
 <template>
-       <li class="bg-slate-100 pt-2 px-2.5 pb-4 w-56 rounded-xl">
-          <div class="my-1 rounded flex items-center justify-center h-52 bg-slate-50">
-            <span class="text-slate-300 text-4xl"></span></div>
-          <p class="text-slate-400 text-xs font-sans leading-5">
-            Lorem ipsum, dolor sit amet this is consectetur adipisicing elit. 
-            dolor sit amet this is consectetur adipisic
-          </p>
-        </li>
+  <li class="bg-slate-100 pt-2 px-2.5 pb-4 w-56 rounded-xl" ref="target">
+    <div class="my-1 rounded flex items-center justify-center h-52 bg-slate-50">
+      <span class="text-slate-300 text-4xl">IMAGE</span>
+    </div>
+    <p class="text-slate-400 text-xs font-sans leading-5">
+      Lorem ipsum, dolor sit amet this is consectetur adipisicing elit.
+      dolor sit amet this is consectetur adipisic
+    </p>
+  </li>
 </template>
 
 <style>
 
-
 </style>
+
--- src/components/CourseSection.vue
+++ src/components/CourseSection.vue
@@ -57,12 +57,13 @@ createServer({
      <div class="custom-container px-16">
        <h2 class="text-2xl text-slate-600 mb-4">Our Courses</h2>
        <ul class="flex items-center flex-wrap gap-5">
-        <li class="bg-slate-100 pt-2 px-2.5 pb-4 w-56 rounded-xl" v-if="!state.loading" v-for="card in state.cards" :key="card.id">
+        <LoadedCard />
+        <!-- <li class="bg-slate-100 pt-2 px-2.5 pb-4 w-56 rounded-xl" v-if="!state.loading" v-for="card in state.cards" :key="card.id">
           <div class="my-1 rounded flex items-center justify-center h-52 bg-slate-50">
             <span class="text-slate-300 text-4xl">{{ card.image }}</span></div>
           <p class="text-slate-400 text-xs font-sans leading-5">{{card.description}}</p>
-        </li>
-        <li class="bg-slate-100 pt-2.5 px-2 pb-5 w-56 rounded-md" v-if="state.loading" v-for="card in state.cards" :key="card.id">
+        </li> -->
+        <!-- <li class="bg-slate-100 pt-2.5 px-2 pb-5 w-56 rounded-md" v-if="state.loading" v-for="card in state.cards" :key="card.id">
           <div class="mb-2 rounded flex items-center justify-center h-52 bg-slate-300"></div>
           <div class="flex flex-wrap gap-1">
             <span class="bg-slate-300 w-36 rounded-full h-4"></span>
@@ -73,7 +74,7 @@ createServer({
             <span class="bg-slate-300 w-20 h-4 rounded-full"></span>
             <span class="bg-slate-300 w-20 rounded-full h-4"></span>
           </div>
-        </li>
+        </li> -->
        </ul>
      </div>
    </div>
